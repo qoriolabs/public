d6a92a6b525dd904dee986c7c4b064e8e9abfbc9
David Chung <dchung@qoriolabs.com>
2015-11-08 22:03:56 -0800

    Update run.mk

--- aws-vpc/passport/nginx/run.mk
+++ aws-vpc/passport/nginx/run.mk
@@ -1,4 +1,6 @@
 DOCKER_IMAGE=infradash/nginx:85
+DASH_DOMAIN=account.qor.io
+DASH_SERVICE=passport-nginx
 CONFIG=http://qoriolabs.github.io/public/aws-vpc/passport/nginx/dash.json
 HOS_IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
 ZKHOSTS="zk01.qor.io:2181,zk02.qor.io:2181,zk03.qor.io:2181"
@@ -16,18 +18,17 @@ get-dash: start
 	chmod a+x dash
 
 register: get-dash
-	ZOOKEEPER_HOSTS=$(ZKHOSTS) \
+	DASH_ZK_HOSTS=$(ZKHOSTS) \
 	./dash --logtostderr --commit \
-	--writepath=/$(DASH_DOMAIN)/passport-nginx --writevalue=$(HOST_IP):443 \
+	--writepath=/$(DASH_DOMAIN)/$(DASH_SERVICE) --writevalue=$(HOST_IP):443 \
 	registry
-
 
 run-nginx: start
 	echo "Starting up NGINX image: $(DOCKER_IMAGE)"
 	docker run -d --name passport-nginx \
 	-e DASH_ZK_HOSTS=$(ZKHOSTS) \
-	-e DASH_DOMAIN=accounts.qor.io \
-	-e DASH_SERVICE=passport-nginx \
+	-e DASH_DOMAIN=$(DASH_DOMAIN) \
+	-e DASH_SERVICE=$(DASH_SERVICE) \
 	-e DASH_CONFIG_URL=$(CONFIG) \
 	-p 443:443 \
 	$(DOCKER_IMAGE)

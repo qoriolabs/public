8f2d29e0539405b31b892375a6e6bef2351888bc
David Chung <dchung@qoriolabs.com>
2015-11-09 23:24:09 -0800

    examples/ demo for dash agents

---
+++ aws-vpc/hello/run.mk
@@ -0,0 +1,37 @@
+DOCKER_IMAGE=infradash/nginx:85
+DASH_DOMAIN=accounts.qor.io
+DASH_SERVICE=passport-nginx
+CONFIG=http://qoriolabs.github.io/public/aws-vpc/passport/nginx/dash.json
+HOS_IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
+ZKHOSTS="zk01.qor.io:2181,zk02.qor.io:2181,zk03.qor.io:2181"
+DASH_BIN="http://infradash.github.io/public/dash/latest/bin/dash"
+
+all: run-nginx register cleanup
+
+start:
+	echo "Running NGINX for $(DASH_DOMAIN)"
+	-docker stop passport-nginx
+	-docker rm passport-nginx
+
+get-dash: start
+	wget $(DASH_BIN)
+	chmod a+x dash
+
+register: get-dash
+	DASH_ZK_HOSTS=$(ZKHOSTS) \
+	./dash --logtostderr --commit \
+	--writepath=/$(DASH_DOMAIN)/$(DASH_SERVICE) --writevalue=$(HOST_IP):443 \
+	registry
+
+run-nginx: start
+	echo "Starting up NGINX image: $(DOCKER_IMAGE)"
+	docker run -d --name passport-nginx \
+	-e DASH_ZK_HOSTS=$(ZKHOSTS) \
+	-e DASH_DOMAIN=$(DASH_DOMAIN) \
+	-e DASH_SERVICE=$(DASH_SERVICE) \
+	-e DASH_CONFIG_URL=$(CONFIG) \
+	-p 443:443 \
+	$(DOCKER_IMAGE)
+
+cleanup:
+	rm dash
---
+++ aws-vpc/minions/dash/demo.mk
@@ -0,0 +1,39 @@
+DOCKER_IMAGE=infradash/dash:release-1.3-82
+CONFIG="http://qoriolabs.github.io/public/aws-vpc/minions/dash/demo.json"
+HOST_IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
+ZKHOSTS="zk01.qor.io:2181,zk02.qor.io:2181,zk03.qor.io:2181"
+DASH_TAGS=appserver
+NAME=dash
+
+all: run-dash
+
+start:
+	echo "Running DASH for $(DASH_DOMAIN)"
+	-docker stop dash
+	-docker rm dash
+
+run-dash: start
+	echo "Starting up DASH image: $(DOCKER_IMAGE)"
+	sudo docker run -d --name=$(NAME) \
+	-e DASH_DOMAIN=$(DASH_DOMAIN) \
+	-e DASH_TAGS=$(DASH_TAGS) \
+	-e DASH_HOST=$(HOST_IP) \
+	-e DASH_NAME=$(NAME) \
+	-e DASH_ZK_HOSTS=$(ZKHOSTS) \
+	-e DASH_DOCKER_PORT=unix:///var/run/docker.sock -v /var/run/docker.sock:/var/run/docker.sock \
+	-p 25657:25657 -p 25658:25658 $(DOCKER_IMAGE) \
+	-status_topic=mqtt://ops-dev.blinker.io:1883/ops-dev \
+	-config_url=$(CONFIG) -timeout=30s -enable_ui=true agent
+
+no-config: start
+	echo "Starting up DASH image: $(DOCKER_IMAGE)"
+	sudo docker run -d --name=$(NAME) \
+	-e DASH_DOMAIN=$(DASH_DOMAIN) \
+	-e DASH_TAGS=$(DASH_TAGS) \
+	-e DASH_HOST=$(HOST_IP) \
+	-e DASH_NAME=$(NAME) \
+	-e DASH_ZK_HOSTS=$(ZKHOSTS) \
+	-e DASH_DOCKER_PORT=unix:///var/run/docker.sock -v /var/run/docker.sock:/var/run/docker.sock \
+	-p 25657:25657 -p 25658:25658 $(DOCKER_IMAGE) \
+	-status_topic=mqtt://iot.eclipse.org:1883/qoriolabs.com/demo \
+	-timeout=30s -enable_ui=true agent

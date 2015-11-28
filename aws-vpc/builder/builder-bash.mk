ca67bb844948c9a01d5ed6d3389019fa665d7c5a
David Chung <dchung@qoriolabs.com>
2015-11-27 22:17:07 -0800

    Initial builder scripts

---
+++ aws-vpc/builder/builder-bash.mk
@@ -0,0 +1,25 @@
+DOCKER_IMAGE=infradash/builder:125
+DASH_CONFIG_URL ?= http://qoriolabs.github.io/public/aws-vpc/builder/bash.json
+DASH_ZK_HOSTS = "10.20.110.50:2181,10.20.115.50:2181,10.20.110.51:2181"
+HOSTIP ?= `/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
+CONTAINER_NAME ?= builder
+
+all: run-builder
+
+start:
+	echo "Running BUILDER for $(DASH_DOMAIN)"
+	-docker stop $(CONTAINER_NAME)
+	-docker rm $(CONTAINER_NAME)
+
+# Also mount local docker so that it has access to docker daemon for building images, etc.
+run-builder: start
+	echo "Starting up BUILDER image: $(DOCKER_IMAGE)"
+	sudo docker run -d -P --name builder --restart=always \
+	-v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker \
+	-e DASH_DOMAIN=$(DASH_DOMAIN) \
+	-e DASH_SERVICE=$(DASH_SERVICE) \
+	-e DASH_VERSION=$(DASH_VERSION) \
+	-e DASH_ZK_HOSTS=$(DASH_ZK_HOSTS) \
+	-e DASH_CONFIG_URL=$(DASH_CONFIG_URL)
+	$(DOCKER_IMAGE) 
+

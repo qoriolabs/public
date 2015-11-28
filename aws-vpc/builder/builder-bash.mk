73b74961fe920172956eb75ba25ac6d18dd268fd
David Chung <dchung@qoriolabs.com>
2015-11-27 22:30:00 -0800

    fix bad makefile; added missing name

--- aws-vpc/builder/builder-bash.mk
+++ aws-vpc/builder/builder-bash.mk
@@ -1,8 +1,9 @@
-DOCKER_IMAGE=infradash/builder:125
+DOCKER_IMAGE    ?= infradash/builder:125
 DASH_CONFIG_URL ?= http://qoriolabs.github.io/public/aws-vpc/builder/bash.json
-DASH_ZK_HOSTS = "zk01.qor.io:2181,zk02.qor.io:2181,zk03.qor.io:2181"
-HOSTIP ?= `/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
-CONTAINER_NAME ?= builder
+DASH_ZK_HOSTS   ?= zk01.qor.io:2181,zk02.qor.io:2181,zk03.qor.io:2181
+DASH_NAME       ?= builder
+HOSTIP          ?= `/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $$1}'`
+CONTAINER_NAME  ?= builder
 
 all: run-builder
 
@@ -19,6 +20,7 @@ run-builder: start
 	-e DASH_DOMAIN=$(DASH_DOMAIN) \
 	-e DASH_SERVICE=$(DASH_SERVICE) \
 	-e DASH_VERSION=$(DASH_VERSION) \
+	-e DASH_NAME=$(DASH_NAME) \
 	-e DASH_ZK_HOSTS=$(DASH_ZK_HOSTS) \
 	-e DASH_CONFIG_URL=$(DASH_CONFIG_URL) \
 	$(DOCKER_IMAGE) 

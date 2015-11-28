3fbdc00e42a5c9e71b6c855a5222d90eaaf58793
David Chung <dchung@qoriolabs.com>
2015-11-27 22:25:35 -0800

    fix bad makefile; remove --restart=always

--- aws-vpc/builder/builder-bash.mk
+++ aws-vpc/builder/builder-bash.mk
@@ -14,7 +14,7 @@ start:
 # Also mount local docker so that it has access to docker daemon for building images, etc.
 run-builder: start
 	echo "Starting up BUILDER image: $(DOCKER_IMAGE)"
-	docker run -d -P --name builder --restart=always \
+	docker run -d -P --name builder \
 	-v /var/run/docker.sock:/var/run/docker.sock -v `which docker`:/bin/docker \
 	-e DASH_DOMAIN=$(DASH_DOMAIN) \
 	-e DASH_SERVICE=$(DASH_SERVICE) \
